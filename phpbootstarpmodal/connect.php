fb711e7753ac92f13a2719437c29f190436093b8
Yaswant Kumar <yaswantkumar1512@gmail.com>
2024-05-25 15:47:43 +0530

    Update connect.php

--- phpbootstarpmodal/connect.php
+++ phpbootstarpmodal/connect.php
@@ -1,13 +1,7 @@
 <?php
-
-
-
-
 $con=new mysqli('localhost','root','','bootstrapcrud');
-
 if ($con -> connect_errno) {
     echo "Failed to connect to MySQL: " . $con -> connect_error;
     exit();
   }
-
 ?>
